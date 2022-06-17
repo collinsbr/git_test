0d8d3571ac64d87f753d627564bcd3d0925049d5
collinsbr <collins.brende@northeastern.edu>
2022-06-17 14:42:22 -0600

    First commit for git test

---
+++ README.md
@@ -0,0 +1,3 @@
+# Readme for git test
+This is a README for me to test git in command line.
+I have a single file and it is a C program that prints "Hello, world."
---
+++ helloworld.c
@@ -0,0 +1,11 @@
+// Simple file that prints hello world
+
+// Added this comment after staging a version of this file without this comment
+
+#include <stdio.h>
+
+int main()
+{
+  printf("Hello, world\n");
+  return 0;
+}
